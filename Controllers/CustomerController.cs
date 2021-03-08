540b7b2af91215ae66102bf411e349ec40efef66
Admin <Admin@DESKTOP-ECGL61C>
2021-03-08 16:41:59 +0530

    commited

---
+++ Controllers/CustomerController.cs
@@ -0,0 +1,27 @@
+﻿using march08.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace march08.Controllers
+{
+    public class CustomerController : Controller
+    { 
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Customer customer)
+        {
+            return View();
+        }
+        // GET: Customer
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/Customer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace march08.Models
+{ 
+    public class Customer
+    {
+        [Required (ErrorMessage = "Enter Your Id")]
+        public int Id { get; set; }
+
+       
+        public String Name { get; set; }
+
+        public String Phone { get; set; }
+    }
+}
